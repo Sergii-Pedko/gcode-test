bd0cd5c44015a4be69cb11bff5771006091b1ae5
Sergii-Pedko <Sergey.pedko123@gmail.com>
2024-08-08 13:45:20 +0300

    commit 3

--- js/lesson21-dom/test10/index.js
+++ js/lesson21-dom/test10/index.js
@@ -1,12 +1,14 @@
 export let finishForm = ()=> {
   let form = document.querySelector('.login-form'); 
+
   let inpLogin = document.createElement('input');
   inpLogin.setAttribute('type', 'text');
   inpLogin.setAttribute('name', 'login');
   form.prepend(inpLogin);
 
-  let inpPass = document.querySelector('input[type=text]'); 
+  let inpPass = document.querySelector('[name="password"]'); 
   inpPass.setAttribute('type', 'password');
+  //console.log(inpPass);
 };
 
 //finishForm();
