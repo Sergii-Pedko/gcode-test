a3332739a06afa5dd12dfcf055371f2e079539ae
Sergii-Pedko <Sergey.pedko123@gmail.com>
2024-08-08 11:56:39 +0300

    commit 1

---
+++ js/lesson21-dom/test9/index.js
@@ -0,0 +1,26 @@
+export let finishList = () => {
+  let ul = document.querySelector('.list');
+  let li5 = document.querySelector('.special');
+
+  let li1 = document.createElement('li');
+  li1.textContent = '1';
+  ul.prepend(li1);
+
+  let li8 = document.createElement('li');
+  li8.textContent = '8';
+  ul.append(li8);
+
+  let li4 = document.createElement('li');
+  li4.textContent = '4';
+  li5.before(li4);
+
+  let li6 = document.createElement('li');
+  li6.textContent = '6';
+  li5.after(li6);
+};
+
+finishList();
+
+
+
+
