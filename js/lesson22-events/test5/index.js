a534d2e8e88a5efefc420082ee68ceaa93b81515
Sergii-Pedko <Sergey.pedko123@gmail.com>
2024-08-13 17:52:10 +0300

    commit 1

---
+++ js/lesson22-events/test5/index.js
@@ -0,0 +1,11 @@
+// Изначально задание НЕ СООТВЕТСТВУЕТ разметке-html
+
+let input = document.querySelector('.search__input'); 
+let btn = document.querySelector('.search__btn'); 
+
+btn.addEventListener('click', ()=>{
+  console.log(input.value);
+}); 
+
+
+  
